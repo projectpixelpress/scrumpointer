22052b0e05e68a80d0064712d7f625b08b9b0b3e
Matthew Burton <matthew.burton@projectpixelpress.com>
2018-08-24 22:08:38 -0500

    Made Scrum Pointer App

---
+++ ScrumPointer/Assets/Scripts/PointsField.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsField : MonoBehaviour {
+
+	[SerializeField] private Button _lessButton;
+	[SerializeField] private Button _moreButton;
+
+	[SerializeField] private Text _amount;
+
+	[HideInInspector]public float value;
+    
+	void Start () {
+		value = 0f;
+		UpdateText();
+	}
+
+	private void OnEnable()
+	{
+		_lessButton.onClick.AddListener(DecreaseValue);
+		_moreButton.onClick.AddListener(IncreaseValue);
+	}
+
+	private void OnDisable()
+    {
+		_lessButton.onClick.RemoveListener(DecreaseValue);
+		_moreButton.onClick.RemoveListener(IncreaseValue);
+    }
+
+	void IncreaseValue(){
+		if(value == 0f)
+		{
+			value = 0.5f;
+		}
+
+		else if (value == 0.5f)
+        {
+            value = 1;
+        }
+
+		else if (value == 1f)
+        {
+            value = 2;
+        }
+
+		else if (value == 2f)
+        {
+            value = 3;
+        }
+        
+		else if (value == 3f)
+        {
+            value = 5;
+        }
+
+		else if (value == 5f)
+        {
+            value = 8;
+        }
+
+		else if (value == 8f)
+        {
+            value = 13;
+        }
+
+		else if (value == 13f)
+        {
+			return;
+        }
+
+		UpdateText();
+	}
+
+	void DecreaseValue()
+    {
+        if (value == 0f)
+        {
+			return;
+        }
+
+        else if (value == 0.5f)
+        {
+            value = 0;
+        }
+
+        else if (value == 1f)
+        {
+            value = 0.5f;
+        }
+
+        else if (value == 2f)
+        {
+            value = 1;
+        }
+
+        else if (value == 3f)
+        {
+            value = 2;
+        }
+
+        else if (value == 5f)
+        {
+            value = 3;
+        }
+
+        else if (value == 8f)
+        {
+            value = 5;
+        }
+
+        else if (value == 13f)
+        {
+			value = 8;
+        }
+
+        UpdateText();
+    }
+
+	void UpdateText(){
+		_amount.text = value.ToString();
+		PointsManager.Instance.UpdateTotal();
+	}
+}
---
+++ ScrumPointer/Assets/Scripts/PointsManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsManager : MonoBehaviour {
+
+	public static PointsManager Instance;
+
+	[SerializeField] private PointsField _effortField;
+	[SerializeField] private PointsField _riskField;
+	[SerializeField] private PointsField _complexityField;
+	[SerializeField] private PointsField _qaEffortField;
+	[SerializeField] private PointsField _buggingOthersField;
+
+	private float _totalValue;
+	[SerializeField] private Text _totalText;
+
+	private void Awake()
+	{
+		if(Instance == null){
+			Instance = this;
+		}
+	}
+
+	public void UpdateTotal(){
+		_totalValue = _effortField.value + _riskField.value + _complexityField.value + _qaEffortField.value + _buggingOthersField.value;
+		_totalText.text = _totalValue.ToString();
+	}
+}
